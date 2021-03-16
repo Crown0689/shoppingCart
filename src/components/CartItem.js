12d61782b6a4a77da777e80ee6694b2ad4cadcd5
Dominik Krone (SE-A/34) <Dominik.krone@online.de>
2021-03-16 15:53:13 +0100

    Shopping Cart: Iterating Through Items

---
+++ src/components/CartItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const CartItem = (product) =>{
+  
+
+    return (
+        <div className="list-group-item">
+            <div className="row">
+                <div className="col-md-8"> {product.product.name} </div>
+                <div className="col-md-2"> {product.product.priceInCents} </div>
+                <div className="col-md-2"> {product.quantity} </div>
+            </div>
+        </div>
+    )
+}
+
+export default CartItem
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import CartItem from './CartItem'
 
 
-CartItems = () =>{
+const CartItems = (props) =>{
+    const items = props.cartItemsList.map((element)=>{  
+        console.log(element.product)
+       return (<CartItem key={element.id} product={element.product} quantity={element.quantity} />)
+
+})
+
+        
+          
     return (
-        <div className="Container">
-            <h1>CartItems</h1>
+        <div className="container">
+        <h1>Cart Items</h1>
+        <div className="list-group">
+            <div className="list-group-item">
+            <div className="row">
+                <div className="col-md-8">Product</div>
+                <div className="col-md-2">Price</div>
+                <div className="col-md-2">Quantity</div>
+            </div>
+            </div>
+            {items}
+        </div>
         </div>
     )
 }
+
+export default CartItems
