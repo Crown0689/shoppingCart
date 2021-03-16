8dea1364646c18b74271e1b7051b9e4398eae917
Dominik Krone (SE-A/34) <Dominik.krone@online.de>
2021-03-16 13:38:59 +0100

    initial push with first components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CartHeader from './components/CartHeader'
+import CartFooter from './components/CartFooter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CartHeader/>
+      <CartFooter/>
     </div>
   );
 }
---
+++ src/components/CartItems.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+CartItems = () =>{
+    return (
+        <div className="Container">
+            <h1>CartItems</h1>
+        </div>
+    )
+}
